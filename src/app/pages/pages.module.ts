303510e4ebbde8f181b7a9391f7902e96341d14b
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-11 19:12:47 -0500

    Using custom attributes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { GraphOneComponent } from './graph-one/graph-one.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
-import { AppRoutingModule } from '../app-routing.module';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
@@ -23,6 +26,6 @@ import { NopagefoundComponent } from './nopagefound/nopagefound.component';
     PagesComponent,
     NopagefoundComponent,
   ],
-  imports: [CommonModule, SharedModule, AppRoutingModule],
+  imports: [CommonModule, FormsModule, SharedModule, AppRoutingModule],
 })
 export class PagesModule {}
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css'],
 })
-export class ProgressComponent implements OnInit {
-  constructor() {}
+export class ProgressComponent {
+  progress: number = 50;
 
-  ngOnInit(): void {}
+  get getProgress() {
+    return `${this.progress}%`;
+  }
+
+  changeValue(value: number) {
+    if (this.progress >= 100 && value >= 0) {
+      return (this.progress = 100);
+    }
+
+    if (this.progress <= 0 && value < 0) {
+      return (this.progress = 0);
+    }
+
+    return (this.progress += value);
+  }
 }
