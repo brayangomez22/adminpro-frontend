c02a0e2f48395625a5d8f93e0754254a1a44da8b
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-12 12:18:39 -0500

    Sidebar Control Service - Side Menu

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SidebarComponent implements OnInit {
+  menuItems: any[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _sidebarService: SidebarService) {
+    this.menuItems = _sidebarService.menu;
   }
 
+  ngOnInit(): void {}
 }
