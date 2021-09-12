e59338668ac3167b517fccf8527f9256221c2204
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-12 14:10:49 -0500

    Call the unsubscribe

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
 
 import { retry, take, map, filter } from 'rxjs/operators';
 
@@ -8,7 +8,9 @@ import { retry, take, map, filter } from 'rxjs/operators';
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css'],
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy {
+  public intervalSubs: Subscription;
+
   constructor() {
     // this.returnObservable()
     //   .pipe(retry())
@@ -18,7 +20,11 @@ export class RxjsComponent {
     //     () => console.info('Obs Finished')
     //   );
 
-    this.returnInterval().subscribe(console.log);
+    this.intervalSubs = this.returnInterval().subscribe(console.log);
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubs.unsubscribe();
   }
 
   returnInterval(): Observable<number> {
