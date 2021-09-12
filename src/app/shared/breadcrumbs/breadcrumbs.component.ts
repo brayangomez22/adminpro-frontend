eec80718062d0cef9ec633783945ab9fef0fb82d
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-12 14:37:10 -0500

    Breadcrumbs optimizations

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,31 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styles: [],
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
   public title!: string;
+  public titleSubs$: Subscription;
 
   constructor(private router: Router) {
-    this.getDataRoute();
+    this.titleSubs$ = this.getDataRoute().subscribe(({ title }) => {
+      this.title = title;
+      document.title = `AdminPro - ${title}`;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.titleSubs$.unsubscribe();
   }
 
   getDataRoute() {
-    this.router.events
-      .pipe(
-        filter(
-          (event): event is ActivationEnd => event instanceof ActivationEnd
-        ),
-        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
-        map((event: ActivationEnd) => event.snapshot.data)
-      )
-      .subscribe(({ title }) => {
-        this.title = title;
-        document.title = `AdminPro - ${title}`;
-      });
+    return this.router.events.pipe(
+      filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
   }
 }
