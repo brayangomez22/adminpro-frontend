b7a90563e117882db3dd31fbe0098a8ad077c488
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-12 01:46:38 -0500

    Adding CSS classes without using ngClass

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -7,15 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
   public linkTheme = document.querySelector('#theme');
+  public links: any[] | NodeListOf<Element> | undefined;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+  }
 
   changeTheme(theme: string) {
     const url = `./assets/css/colors/${theme}.css`;
 
     this.linkTheme?.setAttribute('href', url);
     localStorage.setItem('theme', url);
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    if (this.links != undefined) {
+      this.links.forEach((elem) => {
+        elem.classList.remove('working');
+        const btnTheme = elem.getAttribute('data-theme');
+        const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+        const currentTheme = this.linkTheme?.getAttribute('href');
+
+        if (btnThemeUrl === currentTheme) {
+          elem.classList.add('working');
+        }
+      });
+    }
   }
 }
