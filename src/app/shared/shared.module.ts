8a5226352bdaab39b7ed4d116c9e71bac000b629
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-12 02:05:16 -0500

    RouterLink - Move to a particular route

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -7,6 +10,6 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 @NgModule({
   declarations: [BreadcrumbsComponent, HeaderComponent, SidebarComponent],
   exports: [BreadcrumbsComponent, HeaderComponent, SidebarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
