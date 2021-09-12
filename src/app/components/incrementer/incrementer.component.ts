65995a08cb81596950d9f677aca25950c953e3f6
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-09-11 20:50:59 -0500

    Polish details of our component increment

--- src/app/components/incrementer/incrementer.component.ts
+++ src/app/components/incrementer/incrementer.component.ts
@@ -29,4 +29,16 @@ export class IncrementerComponent implements OnInit {
     this.progress = this.progress + value;
     this.outputValue.emit(this.progress);
   }
+
+  onChange(newValue: number) {
+    if (newValue >= 100) {
+      this.progress = 100;
+    } else if (newValue <= 0) {
+      this.progress = 0;
+    } else {
+      this.progress = newValue;
+    }
+
+    this.outputValue.emit(this.progress);
+  }
 }
